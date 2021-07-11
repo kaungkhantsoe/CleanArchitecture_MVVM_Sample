c2c02a91ede10d354e3c76dd704029d16492557d
kaungkhantsoe <kaungkhantsoe.2712@gmail.com>
2021-07-11 19:06:21 +0630

    - modified Timber init

--- MyFirstCleanArchitectureApp/app/src/main/java/com/kks/myfirstcleanarchitectureapp/ui/BaseApplication.kt
+++ MyFirstCleanArchitectureApp/app/src/main/java/com/kks/myfirstcleanarchitectureapp/ui/BaseApplication.kt
@@ -2,6 +2,7 @@ package com.kks.myfirstcleanarchitectureapp.ui
 
 import android.app.Application
 import com.kks.myfirstcleanarchitectureapp.BuildConfig
+import com.kks.myfirstcleanarchitectureapp.common.ReleaseTree
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import timber.log.Timber.DebugTree
@@ -14,8 +15,20 @@ class BaseApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+
         if (BuildConfig.DEBUG) {
-            Timber.plant(DebugTree())
+            Timber.plant(object : Timber.DebugTree() {
+                override fun createStackElementTag(element: StackTraceElement): String {
+                    return String.format(
+                        "Class:%s: Line: %s, Method: %s",
+                        super.createStackElementTag(element),
+                        element.lineNumber,
+                        element.methodName
+                    )
+                }
+            })
+        } else {
+            Timber.plant(ReleaseTree())
         }
     }
 }
